af6e40025dec2f56179e42a6ef141a2fff3b8a8d
Afonso <tec.sakovuna@gmail.com>
2021-02-08 17:17:44 +0000

    javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome= "Rogério";
+var idade = 29;
+var idade2 = 18;
+var frase = "japão é o melhor time do mundo"
+ alert(nome + "tem" + idade +" anos");
+alert(idade +idade2);
+console.log(nome);
+console.log(idade * idade2)
+console.log(frase.toLocaleUpperCase();
